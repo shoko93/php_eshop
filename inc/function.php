64ef550d4f64c1b8d1926d1db9f83f458f6e60fb
Shoko Ishigaki <shoko.ishigaki@gmail.com>
2019-01-30 11:52:51 +0900

    Update function.php & header.php

--- inc/function.php
+++ inc/function.php
@@ -6,6 +6,16 @@ function get_categories($db) {
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function get_random_items($db) {
+    $sql = 'SELECT products.id AS id, products.name AS name,
+                   categories.name AS category, price FROM products
+            JOIN categories ON products.category_id = categories.id
+            ORDER BY RANDOM() LIMIT 5';
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
 function add_address ($db, $id, $label, $name_to, $postcode, $prefecture, $address,
                       $phone_id, $is_gift = 0, $comment = null) {
     try {
@@ -45,12 +55,11 @@ function add_phone ($db, $id, $label, $phone_number) {
     return $db->lastInsertId();
 }
 
-function get_product_info ($id) {
-    include('connection.php');
+function get_product_info ($db, $id) {
     // Get common produtc details
     try {
-        $sql = 'SELECT products.name AS name, categories.name AS category,
-                price, release_date FROM products
+        $sql = 'SELECT products.name AS name, categories.name AS category, price
+                FROM products
                 JOIN categories ON categories.id = products.category_id
                 WHERE products.id = ?';
         $stmt = $db->prepare($sql);
@@ -62,10 +71,9 @@ function get_product_info ($id) {
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-function get_product_details ($id, $category) {
-    include('connection.php');
+function get_product_details ($db, $id, $category) {
     try {
-        $sql = 'SELECT * FROM ' . $category . ' WHERE product_id = ?';
+        $sql = "SELECT * FROM $category WHERE product_id = ?";
         $stmt = $db->prepare($sql);
         $stmt->bindValue(1, $id, PDO::PARAM_INT);
         $stmt->execute();
@@ -75,16 +83,55 @@ function get_product_details ($id, $category) {
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-function checkPassword ($user_id, $pass, $db) {
-    try {
-        $sql = 'SELECT password FROM user_login WHERE id=?';
+function get_user($db, $id) {
+    $sql = 'SELECT name, email FROM user_login WHERE id = ?';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(1, $id);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function get_user_address_order($db, $id) {
+    $sql = 'SELECT * FROM user_address WHERE user_id = ? ORDER BY label DESC';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function delete_user_address($db, $id) {
+    $sql = 'DELETE FROM user_address WHERE id = ?';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+}
+
+function delete_user_phone($db, $id) {
+    $sql = 'SELECT COUNT(*) FROM user_address WHERE phone_id = ?';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $num = $stmt->fetchColumn();
+    if ($num == 0) {
+        $sql = 'DELETE FROM user_phone WHERE id = ?';
         $stmt = $db->prepare($sql);
-        $stmt->bindValue(1, $user_id, PDO::PARAM_INT);
+        $stmt->bindValue(1, $id, PDO::PARAM_INT);
         $stmt->execute();
-    } catch (Exception $e) {
-        echo $e->getMessage();
     }
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    $user_pass = $user['password'];
-    return $pass == $user_pass;
+}
+
+function get_user_phone($db, $id) {
+    $sql = 'SELECT phone_number FROM user_phone WHERE id = ?';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(1, $id);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function get_user_address($db, $id) {
+    $sql = 'SELECT * FROM user_address WHERE user_id = ?';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
--- inc/header.php
+++ inc/header.php
@@ -12,11 +12,11 @@
         <li><a href="/index.php">Home</a></li>
         <li><a href="/cart.php">Cart</a></li>
       <?php if (isset($_SESSION['login'])): ?>
-        <li><a href="/user_info.php">Account</a></li>
+        <li><a href="/user/user_info.php">Account</a></li>
         <li><a href="/logout.php">Log out</a></li>
       <?php else: ?>
-        <li><a href="/login.php">Log in</a></li>
-        <li><a href="/register.php">Register</a></li>
+        <li><a href="/login/login.php">Log in</a></li>
+        <li><a href="/register/register_form.php">Register</a></li>
       <?php endif; ?>
         <li><a href="/contact.php">Contact</a></li>
       </ul>
